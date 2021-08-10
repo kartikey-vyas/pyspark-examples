1f50867bb5da895290d10ed55c6f78b892673262
Kartikey Vyas <kartikeyvyas23@gmail.com>
2021-08-10 21:17:27 +1200

    checks for image and pull if not found

--- start_env.sh
+++ start_env.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# Script to pull, spin up pyspark docker container and mount the current directory
+# Author: Kartikey Vyas
 
-# Script to spin up pyspark docker container and mount current directory
+# Check if image has been pulled already
+if [ -n "$(docker image inspect jupyter/pyspark-notebook:latest)" ];
+then 
+    echo "jupyter/pyspark image found"
+else
+    echo "jupyter/pyspark image not found -- pulling from docker hub"
+    docker pull jupyter/pyspark-notebook
+fi
+
 
 docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/home/jovyan/work jupyter/pyspark-notebook
